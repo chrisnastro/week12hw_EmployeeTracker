e71f6ac6d4e1354a2d17c2a480d055f364da3742
chrisnastro <cnastro@icloud.com>
2024-02-21 15:45:10 -0500

    added switch function for main menu

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const mysql = require ("mysql");
+const mysql = require("mysql");
 const db = require(".");
 
 const connection = mysql.createConnection({
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
 
@@ -20,24 +20,48 @@ connection.connect(function(err) {
 
 function startPage() {
     inquirer
-    .createPromptModule({
-        type: "list",
-        choices: [
-            "Add Employee",
-            "Add Department",
-            "Add Role",
-            "View Employees",
-            "View Departments",
-            "View Roles",
-            "Update Employee Role",
-            "Exit"
-        ],
-        message: "Choose an option:",
-        name: "option"
-    })
-    .then(function(result) {
-        console.log(result.option + "selected.");
+        .createPromptModule({
+            type: "list",
+            choices: [
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "View Employees",
+                "View Departments",
+                "View Roles",
+                "Update Employee Role",
+                "Exit"
+            ],
+            message: "Choose an option:",
+            name: "option"
+        })
+        .then(function (result) {
+            console.log(result.option + "selected.");
 
-        switch (result.option)
-    })
+            switch (result.option) {
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View Employees":
+                    viewEmployees();
+                    break;
+                case "View Departments":
+                    viewDepartments();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                default:
+                    quit();
+            }
+        });
 }
