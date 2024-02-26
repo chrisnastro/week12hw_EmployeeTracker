f717e0674d228561665e4bfd2217372fbde04ddd
chrisnastro <cnastro@icloud.com>
2024-02-26 14:43:46 -0500

    started seeds

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (title)
+VALUES ("Accounting"),
+("Sales"),
+("Marketing"),
+("IT"),
+("Distribution");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Head of Security", 25000.00, 2), ("Scientist", 600000.00, 3), ("Engineer", 600000.00, 4), ("Butler", 200000.00, 1), ("CEO", 800000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Happy", "Hogan", 1, 3), ("Anton", "Vanko", 1, 1), ("Tony", "Stark", 3, 2), ("Steve", "Quinn", 5, 2), ("Pepper", "Potts", 5, 2);
